cda28b1ed1624957fbbb40f59df3081dea7e31b1
amjad-12 <amjadbarchini40@gmail.com>
2023-03-12 19:39:54 +0200

    fix the header by adding z-index to it

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,16 +18,19 @@ module.exports = {
       },
       fontSize: {
         pxl: '1.7rem',
+        plan: '2.5rem',
         num: '4rem'
         
       },
       gridTemplateColumns: {
         'features': 'repeat(auto-fit, minmax(18.7rem, .5fr))',
         'feature': '16rem',
-        'advantage': 'repeat(auto-fit, minmax(10rem, 1fr))'
+        'advantage': 'repeat(auto-fit, minmax(10rem, 1fr))',
+        'plans': 'repeat(auto-fit, minmax(25rem, 1fr))'
       },
       gridTemplateRows: {
-        'feature': 'repeat(2, 6.5rem)'
+        'feature': 'repeat(2, 6.5rem)',
+        'plans': 'repeat(4, 1fr)'
       },
       borderRadius: {
         'me': '2.5rem',
